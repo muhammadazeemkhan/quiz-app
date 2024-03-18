4fc50a081dbdeed513f02f49cd356587f1872b66
Muhammad Azeem Khan <121564168+muhammadazeemkhan@users.noreply.github.com>
2024-03-18 21:55:10 +0500

    This Repo Contains code of Quiz App

---
+++ js/quiz.js
@@ -0,0 +1,225 @@
+const quizArr = [
+  {
+    id: 1,
+    question: "Every rational number is:",
+    options: {
+      A: "Whole number",
+      B: "Natural number",
+      C: "Integer",
+      D: "Real number",
+    },
+    answer: "D",
+  },
+  {
+    id: 2,
+    question: "√9 is  __________ number.",
+    options: {
+      A: "A rational",
+      B: "An irrational",
+      C: "Neither rational nor irrational",
+      D: "None of the above",
+    },
+    answer: "A",
+  },
+  {
+    id: 3,
+    question: "x2-2x+1 is a polynomial in:",
+    options: {
+      A: "One Variable",
+      B: "Two Variables",
+      C: "Three variable",
+      D: "None of the above",
+    },
+    answer: "A",
+  },
+  {
+    id: 4,
+    question: "What is the degree of the polynomial √3?",
+    options: {
+      A: "0",
+      B: "1",
+      C: "1/2",
+      D: "2",
+    },
+    answer: "A",
+  },
+  {
+    id: 5,
+    question: "Find the value of 7^2-5^2.",
+    options: {
+      A: "22",
+      B: "23",
+      C: "24",
+      D: "25",
+    },
+    answer: "C",
+  },
+  {
+    id: 6,
+    question: "What is the zero of the polynomial p(x)=cx+d?",
+    options: {
+      A: "-c",
+      B: "d",
+      C: "-d/c",
+      D: "d/c",
+    },
+    answer: "B",
+  },
+  {
+    id: 7,
+    question: "A line joining two endpoints is called:",
+    options: {
+      A: " Line segment",
+      B: "A ray",
+      C: "Parallel lines",
+      D: "Intersecting lines",
+    },
+    answer: "A",
+  },
+  {
+    id: 8,
+    question: "Two angles whose sum is equal to 180° are called:",
+    options: {
+      A: "Vertically opposite angles",
+      B: "Complementary angles",
+      C: "Adjacent angles",
+      D: "Supplementary angles",
+    },
+    answer: "D",
+  },
+  {
+    id: 9,
+    question: "A triangle in which two sides are equal is called:",
+    options: {
+      A: "Scalene triangle",
+      B: "Equilateral triangle",
+      C: "Isosceles triangle",
+      D: "None of the above",
+    },
+    answer: "C",
+  },
+  {
+    id: 10,
+    question: "Point (1,1) lies in quadrant.",
+    options: {
+      A: "I",
+      B: "II",
+      C: "III",
+      D: "IV",
+    },
+    answer: "A",
+  },
+  {
+    id: 11,
+    question: "Which ordered pair satisfy the equation y = 2x:",
+    options: {
+      A: "(1,2)",
+      B: "(2,1)",
+      C: "(2,2)",
+      D: "(0,1)",
+    },
+    answer: "A",
+  },
+  {
+    id: 12,
+    question:
+      "The set of points which lie on the same line are called ........ points.",
+    options: {
+      A: "Collinear",
+      B: "Similar",
+      C: "Common",
+      D: "None of these",
+    },
+    answer: "A",
+  },
+  {
+    id: 13,
+    question:
+      "The plane formed by two straight lines perpendicular to each other is called:",
+    options: {
+      A: "Cartesian plane",
+      B: "Co-ordinate Axes",
+      C: "Plane",
+      D: "None of these",
+    },
+    answer: "A",
+  },
+  {
+    id: 14,
+    question: "If (x–1, y+1) = (0, 0), then (x, y) is:",
+    options: {
+      A: "(1,-1)",
+      B: "(-1,1)",
+      C: "(1,1)",
+      D: "(-1,-1)",
+    },
+    answer: "A",
+  },
+  {
+    id: 15,
+    question: "The point of intersection of two coordinate axes is called:",
+    options: {
+      A: "Origin",
+      B: "Center",
+      C: "X-coordinate",
+      D: "Y-coordinate",
+    },
+    answer: "B",
+  },
+  {
+    id: 16,
+    question: " Imaginary part of -i(3i + 2) is",
+    options: {
+      A: "-2",
+      B: "2",
+      C: "3",
+      D: "-3",
+    },
+    answer: "A",
+  },
+  {
+    id: 17,
+    question: "The conjugate of 2 + 3i is ",
+    options: {
+      A: "2 - 3i",
+      B: "-2 - 3i",
+      C: "-2 + 3i",
+      D: "2 + 3i",
+    },
+    answer: "A",
+  },
+  {
+    id: 18,
+    question: " How many factors of a cubic expression are there?",
+    options: {
+      A: "0",
+      B: "1",
+      C: "2",
+      D: "3",
+    },
+    answer: "D",
+  },
+  {
+    id: 19,
+    question: "Factors of 3x – 3a + xy – ay.",
+    options: {
+      A: "(3+y)(x-a)",
+      B: "(3-y)(x+a)",
+      C: "(3-y)(x-a)",
+      D: "(3+y)(x+a)",
+    },
+    answer: "C",
+  },
+  {
+    id: 20,
+    question:
+      "The product of two algebraic expression is equal to the of their H.C.F and L.C.M.",
+    options: {
+      A: "Sum",
+      B: "Difference",
+      C: "Product",
+      D: "Quotient",
+    },
+    answer: "C",
+  },
+];
